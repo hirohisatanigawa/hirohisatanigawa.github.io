d3483c8ed33cb5171f2c8be454cc82f1a7fcca16
Hirohisa Tanigawa <emptybottlexxx@gmail.com>
2025-06-21 15:25:45 +0900

    Deploy international-school-english app

---
+++ international-school-english/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/international-school-english/sw.js', { scope: '/international-school-english/' })})}
